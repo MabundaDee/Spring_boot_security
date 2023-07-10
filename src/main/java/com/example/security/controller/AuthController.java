8026337226cb56e99129db25272a23aa9ede85a5
MabundaDee <shaydmitry232@gmail.com>
2023-07-10 16:27:07 +0200

    binding

---
+++ src/main/java/com/example/security/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.example.security.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+
+    }
+
